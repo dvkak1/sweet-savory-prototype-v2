3707c20555b39702eb0f06cfa580c3f6d266b330
dvkak1 <dv_kakilala@yahoo.com>
2025-04-22 15:14:45 +0800

    user admin login create

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [HomeController::class, 'index']);
 
 Route::middleware([
     'auth:sanctum',
