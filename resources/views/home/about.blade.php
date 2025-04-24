c3295ee460a635a23558efe20e181da3ceacb036
dvkak1 <dv_kakilala@yahoo.com>
2025-04-24 16:38:37 +0800

    hero and about section modified

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -1,15 +1,20 @@
 <!--  About Section  -->
-<div id="about" class="container-fluid wow fadeIn" id="about"data-wow-duration="1.5s">
+<div id="about" class="container-fluid wow fadeIn" data-wow-duration="1.5s">
     <div class="row">
         <div class="col-lg-6 has-img-bg"></div>
         <div class="col-lg-6">
             <div class="row justify-content-center">
                 <div class="col-sm-8 py-5 my-5">
-                    <h2 class="mb-4">About Us</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, quisquam accusantium nostrum modi, nemo, officia veritatis ipsum facere maxime assumenda voluptatum enim! Labore maiores placeat impedit, vero sed est voluptas!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita alias dicta autem, maiores doloremque quo perferendis, ut obcaecati harum, <br><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum necessitatibus iste,
-                    nulla recusandae porro minus nemo eaque cum repudiandae quidem voluptate magnam voluptatum? <br>Nobis, saepe sapiente omnis qui eligendi pariatur. quis voluptas. Assumenda facere adipisci quaerat. Illum doloremque quae omnis vitae.</p>
-                    <p><b>Lonsectetur adipisicing elit. Blanditiis aspernatur, ratione dolore vero asperiores explicabo.</b></p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ab itaque modi, reprehenderit fugit soluta, molestias optio repellat incidunt iure sed deserunt nemo magnam rem explicabo vitae. Cum, nostrum, quidem.</p>
+                    <h2 class="mb-4">About Sweet & Savory Fusion</h2>
+                    <p>
+                        Sweet & Savory Fusion is a cozy, home-based bakeshop from <strong>Cabanatuan City</strong>,
+                        lovingly baking handcrafted pastries for locals and sweet tooths in <strong>Metro Manila</strong> too.
+                    </p>
+                    <p>
+                        From rich cakes to savory bites, every treat is made with heart —
+                        and delivered to your doorstep, just like it came from our kitchen to yours. 💛
+                    </p>
+                    <p><b>Order today and experience the fusion of flavors made with love and warmth.</b></p>
                 </div>
             </div>
         </div>
--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -1,8 +1,10 @@
- <!-- Navbar -->
- <nav class="custom-navbar navbar navbar-expand-lg navbar-dark fixed-top" data-spy="affix" data-offset-top="10">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<!-- Navbar -->
+<nav class="custom-navbar navbar navbar-expand-lg navbar-dark fixed-top" data-spy="affix" data-offset-top="10">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
+
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav">
             <li class="nav-item">
@@ -12,19 +14,21 @@
                 <a class="nav-link" href="#about">About</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#gallary">Gallary</a>
+                <a class="nav-link" href="#gallary">Gallery</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#book-table">Book-Table</a>
+                <a class="nav-link" href="#book-table">Order Now</a>
             </li>
         </ul>
+
         <a class="navbar-brand m-auto" href="#">
             <img src="assets/imgs/logo.svg" class="brand-img" alt="">
-            <span class="brand-txt">Food Hut</span>
+            <span class="brand-txt">Sweet & Savory Fusion</span>
         </a>
+
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="#blog">Blog<span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="#blog">Blog</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#testmonial">Reviews</a>
@@ -33,16 +37,17 @@
                 <a class="nav-link" href="#contact">Contact Us</a>
             </li>
             <li class="nav-item">
-                <a href="components.html" class="btn btn-primary ml-xl-4">Components</a>
+                <a href="components.html" class="btn btn-primary ml-xl-4">Menu</a>
             </li>
         </ul>
     </div>
 </nav>
-<!-- header -->
+
+<!-- Header -->
 <header id="home" class="header">
     <div class="overlay text-white text-center">
-        <h1 class="display-2 font-weight-bold my-3">Food Hut</h1>
-        <h2 class="display-4 mb-5">Always fresh &amp; Delightful</h2>
-        <a class="btn btn-lg btn-primary" href="#gallary">View Our gallary</a>
+        <h1 class="display-2 font-weight-bold my-3">Sweet & Savory Fusion</h1>
+        <h2 class="display-4 mb-5">Baked with love, delivered with care</h2>
+        <a class="btn btn-lg btn-primary" href="#gallary">View Our Creations</a>
     </div>
 </header>
